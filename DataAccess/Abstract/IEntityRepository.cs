938a47d491c302655365d03485b1678cdaabae3e
pc <pc@DESKTOP-4BEBRK9>
2021-07-26 18:19:24 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+
+
+    //Generic Constraint
+    // class: referans tip olabilir 
+    // IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    // new() : new'lenebilir olmalı çünkü IEntity newlenemez interface olduğu için 
+    //ARTIK SİSTEMİMİZ SADECE VERİTABANI NESNELERİYELE ÇALIŞAN REPOSİTOR OLDU 
+   public interface IEntityRepository<T> where T:class , IEntity,new() // REFERANS TİP OLMASI GEREKİR ve IEntity olabilir yada IEntityden implementesinden olması gerekiyor
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+
+
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    // public yaptığımızda bu  class'a diğer katmanlarda ulaşabilsin diye ekliyoruz
+   public class Product: IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+    }
+}
